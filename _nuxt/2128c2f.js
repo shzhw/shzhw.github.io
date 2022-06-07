660b16e558916a258223392ec03e9d156f4c225a
shizw <zws@zwdeMacBook-Pro.local>
2022-06-07 16:58:49 +0800

    dist

---
+++ _nuxt/2128c2f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{467:function(e,t,n){"use strict";n.r(t);n(26),n(94);var r={beforeCreate:function(){this.$router.replace({name:"member-list"})}},c=n(15),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"8df5e444",null);t.default=component.exports}}]);
